2c46f9b8b1ef603f06ecc965555e571eb784e41c
Muhammad Dandi Permana <93873807+Muhammaddandipermana@users.noreply.github.com>
2022-10-08 09:28:03 +0700

    Update Kemuning.js

--- src/components/klasifikasiTanaman/KlasifikasiSemakSedang/Kemuning.js
+++ src/components/klasifikasiTanaman/KlasifikasiSemakSedang/Kemuning.js
@@ -9,7 +9,7 @@ function BasicExample() {
     <div className="row">
               <div className="col-md-6">
                 <div className="single-image">
-                <img src="/images/SemakSedang/Kemuning.jpg" width={'262px'} height={'196px'} alt="Third slide "
+                <img src="/images/SemakSedang/kemuning.jpg" width={'262px'} height={'196px'} alt="Third slide "
                 />
                 </div>
               </div>
