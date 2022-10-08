aeb25b8bd2d3b6b8aa32c930b7a00eb6f0db6ba3
Muhammad Dandi Permana <93873807+Muhammaddandipermana@users.noreply.github.com>
2022-10-08 09:30:20 +0700

    Update Melati.js

--- src/components/klasifikasiTanaman/KlasifikasiSemakSedang/Melati.js
+++ src/components/klasifikasiTanaman/KlasifikasiSemakSedang/Melati.js
@@ -9,7 +9,7 @@ function BasicExample() {
     <div className="row">
               <div className="col-md-6">
                 <div className="single-image">
-                <img src="/images/SemakSedang/Melati.jpg" width={'262px'} height={'196px'} alt="Third slide "
+                <img src="/images/SemakSedang/melati.jpg" width={'262px'} height={'196px'} alt="Third slide "
                 />
                 </div>
               </div>
